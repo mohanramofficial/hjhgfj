3e2413149560eec02b320144b5899c339d5d3d08
mohanramofficial <36396379+mohanramofficial@users.noreply.github.com>
2018-03-27 16:39:05 +0530

    Create 88.py

---
+++ 88.py
@@ -0,0 +1,13 @@
+def lcm2():
+	(x,y)=map(int,sys.stdin.readline().split())
+	temp=min(x,y)
+	while(True):
+		if temp%x==0 and temp%y==0:
+			break
+		temp+=1
+	print(temp)
+try:
+	lcm2()
+except:
+	print('invalid')
+  p
